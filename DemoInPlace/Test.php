90ba1412e77706e2ad1ae737af5166a1e0617d36
Andreas Martin <andreas.martin@andreasmartin.ch>
2016-09-05 16:15:30 +0200

    Initial commit

---
+++ DemoInPlace/Test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreas.martin
+ * Date: 30.08.2016
+ * Time: 10:28
+ */
+for($i=0;$i<10;$i++)
+{
+    echo "Hallo ".$i;
+}
---
+++ DemoLocalFolder/Test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreas.martin
+ * Date: 01.09.2016
+ * Time: 10:14
+ */
+echo "Test";
